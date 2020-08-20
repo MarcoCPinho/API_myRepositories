c334d1136532d0109d3f57dcfc892ec017e39c5c
MarcoCPinho <marcocpinho@hotmail.com>
2020-08-20 13:59:19 -0300

    alt

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid } = require('uuid');
+const { v4: uuid } = require('uuid');
 
 const app = express();
 
@@ -10,12 +10,50 @@ app.use(cors());
 
 const repositories = [];
 
+function logRequests(request, response, next) {
+  const { method, url } = request;
+
+  const logLabel = `[${method.toUpperCase()}] ${url}`;
+
+  console.time(logLabel);
+
+  next(); //tiro o return para ele executar o código abaixo
+
+  console.timeEnd(logLabel);
+}
+
+function validadeRepoId(request, response, next) {
+  const { id } = request.params;
+
+  if (!uuidValidate(id)) {
+    return response.status(400).json({ error: 'Invalid repositorie ID.' })
+  }
+
+  return next();
+}
+
+app.use(logRequests);
+app.use('/repositories/:id', validadeRepoId);
+
 app.get("/repositories", (request, response) => {
-  // TODO
+
+  const { title, techs } = request.query;
+
+  const result = title || techs
+    ? repositories.filter(repoData => repoData.title.includes(title) || repoData.techs.includes(techs))
+    : repositories;
+
+  return response.json(result);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs, likes = 0 } = request.body;
+
+  const repoData = { id: uuid(), title, url, techs, likes };
+
+  repositories.push(repoData);
+
+  return response.json(repoData);
 });
 
 app.put("/repositories/:id", (request, response) => {
